ab7941ccb5db580ba776ab971fcda12372d4e641
LimWeiMing0604 <Lim.Softengineer@gmail.com>
2023-08-26 02:52:56 -0700

    Create typings.d - Copy.ts
    asd

---
+++ typings.d - Copy.ts
@@ -0,0 +1,8 @@
+declare module '*.css';
+declare module '*.less';
+declare module "*.png";
+
+declare module "components";
+declare module "components/*";
+declare module "utils";
+declare module "config/*";
